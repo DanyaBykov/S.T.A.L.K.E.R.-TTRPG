76a2b28719928140a803ac56a4ee167303439003
Danya Bykov <bykov.danya.ua@gmail.com>
2025-04-07 11:38:52 +0300

    cleaned up the structure

--- src/Inventory/main.jsx
+++ src/Inventory/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './InventorySystem.css'
+import InventorySystem from './InventorySystem'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <InventorySystem />
   </StrictMode>,
 )
