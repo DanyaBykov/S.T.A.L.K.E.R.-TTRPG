dfdac83362e5871bdb187fca30351ee4a892c303
Ivan Sen <senivan1997@gmail.com>
2025-05-18 18:04:38 +0300

    Fixed api definition

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://stalker.ucuserver.cc/';
+const API_URL = 'https://stalker.ucuserver.cc';
 
 // Helper function for fetch requests
 async function apiRequest(url, options = {}) {
